96e4c33890a6f64e18ac5558897748355417444d
=Kevin <nutkey5@gmail.com>
2020-04-08 09:33:57 -0400

    f

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,8 @@ const router  = express.Router();
 const api = require('./api/api');
 const docs = require('./docs');
 
+const data = require('covidtracker');
+
 const discord = require('../discord/index');
 
 router.use('/api', api);
@@ -11,6 +13,27 @@ router.use('/doc', docs);
 
 router.get('/', async (req, res) => {
     res.render('index');
+});
+
+router.get('/covid', async(req, res) => {
+    let state = await data.getState({ state: 'Kentucky' });
+    JSON.stringify(state);
+    
+    let cases = state.cases;
+    let todayCases = state.todayCases;
+    let deaths = state.deaths;
+    let todayDeaths = state.todayDeaths;
+    let active = state.active;
+    let tests = state.tests;
+
+    res.render('covid',{
+        cases: cases,
+        todayCases: todayCases,
+        deaths: deaths,
+        todayDeaths: todayDeaths,
+        active: active,
+        tests: tests
+    });
 });
 
 router.get('/api/discord', async (req, res) => {
