68811f1f91a31bd667789dde7df9c0b0c4c9c52e
Kevin <nutkey5@gmail.com>
2020-04-07 23:09:05 -0400

    Add files via upload

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router  = express.Router();
 
+const discord = require('../discord/index');
+
 router.get('/', async(req, res) => {
     res.render('index');
+});
+
+router.get('/discord', async(req, res) => {
+    res.json({
+        "status": discord.getStatus(),
+        "ping": discord.getPing()
+    });
 });
 
 module.exports = router;
