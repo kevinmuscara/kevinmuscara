89389c41b932c015ec6e82a5dcd65b2df664a27c
=Kevin <nutkey5@gmail.com>
2020-04-11 06:22:56 -0400

    routing change

--- src/routes/docs.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router  = new express.Router();
-
-router.get('/', async(req, res) => {
-    res.render('doc');
-});
-
-module.exports = router;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,56 +1,268 @@
 const express = require('express');
 const router  = express.Router();
 
-const api = require('./api/api');
-const docs = require('./docs');
-
 const data = require('covidtracker');
 
 const discord = require('../discord/index');
 
-router.use('/api', api);
-router.use('/doc', docs);
+router.get('/doc', async(req, res) => {
+    res.render('doc');
+});
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {    
     const head = req.headers.host;
-    // console.log(head);
+
     if(head === 'kevinmuscara.com')
         res.render('index');
     else if(head === 'api.kevinmuscara.com')
         res.render('doc');
-
-    // res.render('index');
 });
 
 router.get('/covid', async(req, res) => {
-    let state = await data.getState({ state: 'Kentucky' });
-    JSON.stringify(state);
-    
-    let name = state.state;
-    let cases = state.cases;
-    let todayCases = state.todayCases;
-    let deaths = state.deaths;
-    let todayDeaths = state.todayDeaths;
-    let pactive = state.active / state.cases;
-    let pdeaths = state.deaths / state.cases;
+    const header = req.headers.host;
 
-    res.render('covid',{
-        state: name,
-        cases: cases,
-        todayCases: todayCases,
-        deaths: deaths,
-        todayDeaths: todayDeaths,
-        dead: Math.round(pdeaths * 100),
-        infected: Math.round(pactive * 100)
-    });
+    if(header === 'kevinmuscara.com') {
+        let state = await data.getState({ state: 'Kentucky' });
+        JSON.stringify(state);
+        
+        let name = state.state;
+        let cases = state.cases;
+        let todayCases = state.todayCases;
+        let deaths = state.deaths;
+        let todayDeaths = state.todayDeaths;
+        let pactive = state.active / state.cases;
+        let pdeaths = state.deaths / state.cases;
+    
+        res.render('covid',{
+            state: name,
+            cases: cases,
+            todayCases: todayCases,
+            deaths: deaths,
+            todayDeaths: todayDeaths,
+            dead: Math.round(pdeaths * 100),
+            infected: Math.round(pactive * 100)
+        });
+    } else if(header === 'api.kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
 });
 
-router.get('/api/discord', async (req, res) => {
-    const message = JSON.stringify({ "status": discord.getStatus(), "ping": discord.getPing() });
+router.get('/discord', async (req, res) => {
+    const header = req.headers.host;
 
-    res.render('message', {
-        msg: message
-    });
+    if(header === 'api.kevinmuscara.com') {
+        const message = JSON.stringify({ "status": discord.getStatus(), "ping": discord.getPing() });
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/player/:username', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/bans', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/health', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+        String.prototype.toHHMMSS = function() {
+            var second_number = parseInt(this, 10);
+            var hours = Math.floor(second_number / 3600);
+            var minutes = Math.floor((second_number - (hours * 3600)) / 60);
+            var seconds = second_number - (hours * 3600) - (minutes * 60);
+
+            if(hours < 10) { hours = "0" + hours; }
+            if(minutes < 10) { minutes = "0" + minutes; }
+            if(seconds < 10) { seconds = "0" + seconds; }
+            const time = `${hours}:${minutes}:${seconds}`;
+            return time;
+        }
+
+        const time = process.uptime();
+        const uptime = (time + "").toHHMMSS();
+
+
+        const message = JSON.stringify({ "status": "online", "uptime": uptime });
+
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/corona', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+        let world = await data.getAll();
+        const message = JSON.stringify({ "deaths": world.deaths, "confirmedCases": world.cases, "totalRecovered": world.recovered});
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/corona/:country', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+        let country = await data.getCountry({ country: req.params.country });
+        const message = JSON.stringify({ "deaths": country.deaths, "confirmedCases": country.cases, "totalRecovered": country.recovered});
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/corona/:country/deaths', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+        let country = await data.getCountry({ country: req.params.country });
+        const message = JSON.stringify({ "deaths": country.deaths });
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/corona/:country/confirmedCases', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+        let country = await data.getCountry({ country: req.params.country });
+        const message = JSON.stringify({ "confirmedCases": country.cases });
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/corona/:country/totalRecovered', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+        let country = await data.getCountry({ country: req.params.country });
+        const message = JSON.stringify({ "totalRecovered": country.recovered });
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/corona/:country/:state', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+        let state = await data.getState({ state: req.params.state });
+        const message = JSON.stringify({ "deaths": state.deaths, "confirmedCases": state.cases, "totalRecovered": state.recovered });
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/corona/:country/:state/deaths', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+        let state = await data.getState({ state: req.params.state });
+        const message = JSON.stringify({ "deaths": state.deaths });
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/corona/:country/:state/confirmedCases', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+        let state = await data.getState({ state: req.params.state });
+        const message = JSON.stringify({ "confirmedCases": state.cases });
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
+});
+
+router.get('/corona/:country/:state/totalRecovered', async(req, res) => {
+    const header = req.headers.host;
+
+    if(header === 'api.kevinmuscara.com') {
+        let state = await data.getState({ state: req.params.state });
+        const message = JSON.stringify({ "totalRecovered": state.recovered });
+        res.render('message', {
+            msg: message
+        });
+    } else if(header === 'kevinmuscara.com') {
+        res.render('message', {
+            msg: '404 ERROR'
+        });
+    }
 });
 
 module.exports = router;
